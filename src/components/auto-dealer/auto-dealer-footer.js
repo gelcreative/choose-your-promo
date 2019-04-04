91d36c6bd76823cd5658295f840875245d735da0
Gel Studio <devondaviau@yahoo.ca>
2019-04-04 16:02:53 -0400

    style footer text

--- src/components/auto-dealer/auto-dealer-footer.js
+++ src/components/auto-dealer/auto-dealer-footer.js
@@ -4,7 +4,32 @@ import styled from 'styled-components'
 const StyledFooter = styled.footer`
   background: ${props => props.footer.footerBgColor};
   color: ${props => props.footer.footerTxtColor};
-  padding: 100px 0;
+  padding-top: 100px;
+  padding-bottom: 100px;
+
+  .choose-your-promo-attrib p {
+    font-size: 1.4rem;
+    text-transform: uppercase;
+    margin-bottom: 3rem;
+    span {
+      display: inline-block;
+      font-size: 1.8rem;
+      font-weight: 600;
+      margin-top: 1.6rem;
+    }
+  }
+
+  .choose-your-promp-terms {
+    p {
+      font-size: 1.4rem;
+      margin-bottom: 1.6rem;
+      text-transform: uppercase;
+    }
+
+    small {
+      font-size: 1.2rem;
+    }
+  } 
 `
 
 const Footer = ({ data }) => {
@@ -15,13 +40,16 @@ const Footer = ({ data }) => {
         <div className="columns">
           <div className="column">
             <div className="columns">
-              <div className="column">
-                <p className="choose-your-promo-attrib">
+              <div className="column choose-your-promo-attrib">
+                <p>
                   Powered by: <br />
                   <span>chooseyourpromo.com</span>
                 </p>
               </div>
-              <div className="column"></div>
+              <div className="column choose-your-promp-terms">
+                <p>Terms and Conditions Apply</p>
+                <small>{footer.footerTermsTxt}</small>
+              </div>
             </div>
           </div>
           <div className="column"></div>
