cb5ba29342e11bbf8ec4a79bbd76163ccd3dc0b4
Gel Studio <devondaviau@yahoo.ca>
2019-04-05 17:09:54 -0400

    change netlify attribute

--- src/components/auto-dealer/auto-dealer-form.js
+++ src/components/auto-dealer/auto-dealer-form.js
@@ -202,7 +202,7 @@ class AutoDealerForm extends Component {
                 <h1>Receive Your {ReactHtmlParser(this.state.promo)}</h1>
               </div>
             </div>
-            <form onSubmit={this.handleSubmit} netlify>
+            <form onSubmit={this.handleSubmit} name="promoForm" data-netlify="true">
               <div className="columns">
                 <div className="column">
                   <label htmlFor="first-name" className="visually-hidden">Your First Name</label>
